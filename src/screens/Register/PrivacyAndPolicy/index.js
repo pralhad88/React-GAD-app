62d3867ec16bc2388d15ca3f6e7c18020625d0e9
pralhad88 <pralhad18@navgurukul.org>
2020-02-15 11:18:11 +0530

    terms and policy added

--- src/screens/Register/PrivacyAndPolicy/index.js
+++ src/screens/Register/PrivacyAndPolicy/index.js
@@ -25,7 +25,6 @@ const styles = theme => ({
   },
   submit: {
     width: 100,
-    marginLeft: '34%',
     color: 'white',
   },
 });
@@ -62,14 +61,52 @@ class PrivacyAndPolicy extends Component {
               <hr></hr>
             </Box>
             <Box my={2}>
-              {[...new Array(22)]
-                .map(
-                  () => `Cras mattis consectetur purus sit amet fermentum.
-                      Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-                      Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-                      Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-                )
-                .join('\n')}
+              <p> Last updated: February 07, 2019</p>
+              <p> Navision Limited ("us", "we", or "our") operates the giftadeed mobile app (the "Service"). This page informs you of our policies regarding the collection, use and disclosure of Personal Information when you use our Service.</p>
+              <p> We will not use or share your information with anyone except as described in this Privacy Policy. This Privacy Policy is licensed by TermsFeed Generator to Gift-a-Deed.</p>
+              <p> We use your Personal Information for providing and improving the Service. By using the Service, you agree to the collection and use of information in accordance with this policy. Unless otherwise defined in this Privacy Policy, terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, accessible at Gift-a-Deed mobile app.</p>
+            </Box>
+            <Box my={2}>
+              <Typography variant='h6'>Information Collection And Use</Typography>
+              <p> While using our service, we may ask you to provide us with certain personally identifiable information that can be used to contact or identify you. Personally identifiable information may include, but is not limited to, your name, phone number, postal address, other information ("Personal Information").</p>
+              <p> Considering the fact that the primary purpose of this app is to address <span style={{backgroundColor: 'yellow'}}>humanitarian needs,</span> users can also make use of this app for <span style={{backgroundColor: 'yellow'}}>Emergency situations</span> that endanger life and property. This app uses device features like <span style={{backgroundColor: 'yellow'}}>Messaging, Contacts and Call in emergency situations.</span></p>
+            </Box>
+            <Box my={2}>
+              <Typography variant='h6'>Log Data</Typography>
+              <p> We collect information that your mobile sends whenever you visit our service ("Log Data"). This Log Data may include information such as your device’s Internet Protocol ("IP") address, browser type, browser version, the pages of our service that you visit, the time and date of your visit, the time spent on those pages and other statistics.</p>
+            </Box>
+            <Box my={2}>
+              <Typography variant='h6'>Cookies</Typography>
+              <p> Cookies are files with small amount of data, which may include an anonymous unique identifier. Cookies are sent to your browser from a web site and stored on your computer's hard drive.</p>
+              <p> We use "cookies" to collect information. You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept cookies, you may not be able to use some portions of our Service.</p>
+            </Box>
+            <Box my={2}>
+              <Typography variant='h6'>Service Providers</Typography>
+              <p> We may employ third party companies and individuals to facilitate our Service, to provide the service on our behalf, to perform Service-related services or to assist us in analyzing how our service is used.</p>
+              <p> These third parties have access to your Personal Information only to perform these tasks on our behalf and are obligated not to disclose or use it for any other purpose. </p>
+            </Box>
+            <Box my={2}>
+              <Typography variant='h6'>Security</Typography>
+              <p> The security of your Personal Information is important to us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While we strive to use commercially acceptable means to protect your Personal Information, we cannot guarantee its absolute security.</p>
+            </Box>
+            <Box my={2}>
+              <Typography variant='h6'>Links To Other Sites</Typography>
+              <p> Our service may contain links to other sites that are not operated by us. If you click on a third party link, you will be directed to that third party's site. We strongly advise you to review the Privacy Policy of every site you visit.</p>
+              <p> We have no control over, and assume no responsibility for the content, privacy policies or practices of any third party sites or services.</p>
+            </Box>
+            <Box my={2}>
+              <Typography variant='h6'>Children's Privacy</Typography>
+              <p> Our service does not address anyone under the age of 13 ("Children").</p>
+              <p> We do not knowingly collect personally identifiable information from children under 13. If you are a parent or guardian and you are aware that your Children has provided us with Personal Information, please contact us. If we discover that a Children under 13 has provided us with Personal Information, we will delete such information from our servers immediately.</p>
+            </Box>
+            <Box my={2}>
+              <Typography variant='h6'>Changes To This Privacy Policy</Typography>
+              <p> We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.</p>
+              <p> You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.</p>
+            </Box>
+            <Box my={2}> 
+              <Typography variant='h6'>Contact Us</Typography>
+              <p>If you have any questions about this Privacy Policy, please contact us.</p>
             </Box>
             <Grid container spacing={3}>
               <Grid item xs={12}>
--- src/screens/Register/TermAndConditions/index.js
+++ src/screens/Register/TermAndConditions/index.js
@@ -25,7 +25,6 @@ const styles = theme => ({
   },
   submit: {
     width: 100,
-    marginLeft: '34%',
     color: 'white',
   },
 });
@@ -62,14 +61,46 @@ class TermsAndConditions extends Component {
               <hr></hr>
             </Box>
             <Box my={2}>
-              {[...new Array(20)]
-                .map(
-                  () => `Cras mattis consectetur purus sit amet fermentum.
-                      Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-                      Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-                      Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-                )
-                .join('\n')}
+              <p>Last updated: February 07, 2019</p>
+              <p>Please read these Terms and Conditions ("Terms", "Terms and Conditions") carefully before using the gift-a-deed mobile app (the "Service") operated by Navision Limited ("us", "we", or "our").</p>
+              <p>Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who access or use the Service.</p>
+              <p>By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service. This Terms & Conditions agreement is licensed by TermsFeed to gift-a-deed.</p>
+              <p>Donors/Fulfiller offer and distribute food at their own risk and responsibility. Gift-a-Deed and none of its employees or management is responsible for food offering and distribution.</p>
+              <p>Also, Gift-a-Deed and none of its employees or management is responsible for any misuse of SOS feature.</p>
+            </Box>
+            <Box my={2}>
+              <Typography variant="h6">Accounts</Typography>
+              <p>When you create an account with us, you must provide us information that is accurate, complete, and current at all times. Failure to do so constitutes a breach of the Terms, which may result in immediate termination of your account on our Service.</p>
+              <p> You are responsible for safeguarding the password that you use to access the Service and for any activities or actions under your password, whether your password is with our Service or a third-party service.</p>
+              <p> You agree not to disclose your password to any third party. You must notify us immediately upon becoming aware of any breach of security or unauthorized use of your account.</p>
+            </Box>
+            <Box my={2}>
+              <Typography variant="h6">Links To Other Apps</Typography>
+              <p>Our Service may contain links to third-party mobile apps or services that are not owned or controlled by gift-a-deed.</p>
+              <p>gift-a-deed has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party mobile apps or services. You further acknowledge and agree that gift-a-deed shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such mobile apps or services.</p>
+              <p> We strongly advise you to read the terms and conditions and privacy policies of any third-party mobile apps or services that you visit.</p>
+            </Box>
+            <Box my={2}>
+              <Typography variant="h6">Termination</Typography>
+              <p>We may terminate or suspend access to our Service immediately, without prior notice or liability, for any reason whatsoever, including without limitation if you breach the Terms.</p>
+              <p> All provisions of the Terms which by their nature should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity and limitations of liability.</p>
+              <p> We may terminate or suspend your account immediately, without prior notice or liability, for any reason whatsoever, including without limitation if you breach the Terms.</p>
+              <p> Upon termination, your right to use the Service will immediately cease. If you wish to terminate your account, you may simply discontinue using the Service.</p>
+              <p> All provisions of the Terms which by their nature should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity and limitations of liability.</p>
+            </Box>
+            <Box my={2}>
+              <Typography variant="h6">Governing Law</Typography>
+              <p>These Terms shall be governed and construed in accordance with the laws of Hong Kong, India, U.K., U.S.A., Singapore without regard to its conflict of law provisions.</p>
+              <p> Our failure to enforce any right or provision of these Terms will not be considered a waiver of those rights. If any provision of these Terms is held to be invalid or unenforceable by a court, the remaining provisions of these Terms will remain in effect. These Terms constitute the entire agreement between us regarding our Service, and supersede and replace any prior agreements we might have between us regarding the Service.</p>
+            </Box>
+            <Box my={2}>
+              <Typography variant="h6">Changes</Typography>
+              <p>We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material we will try to provide at least 15 days notice prior to any new terms taking effect. What constitutes a material change will be determined at our sole discretion.</p>
+              <p> By continuing to access or use our Service after those revisions become effective, you agree to be bound by the revised terms. If you do not agree to the new terms, please stop using the Service.</p>
+            </Box>
+            <Box my={2}>
+              <Typography variant="h6">Contact Us</Typography>
+              <p>If you have any questions about these Terms, please contact us.</p>
             </Box>
             <Grid container spacing={3}>
               <Grid item xs={12}>
